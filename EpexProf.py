b5a6e42d1fdf826afc1beea0a68e122fcba96cde
AggelosNtoumi <64909643+AggelosNtoumi@users.noreply.github.com>
2020-05-22 18:25:24 +0300

    Add files via upload

---
+++ EpexProf.py
@@ -0,0 +1,32 @@
+class Traveler:
+    import cv2
+    name= "Mickey"
+    surname = "Mouse"
+    username = "mickeymouse"
+    password="123456789"
+    birthdate = "03/04/1998"
+    telephone = "6969696969"
+    email = "mickeymouse@upatras.com"
+    profilePicture = cv2.imread('mickeyspicture.png')
+    enableTraveling = False
+    travelBuddies = ['Donald Duck', 'Goofy']
+    
+    
+    def SelectPendingTravels():
+        //   
+    def PostReview():
+        //
+    def EnterPassword():
+        //
+    def PasswordVerification():
+        //
+    def EditInfo():
+        //
+    def DisplayInfo():
+        //
+    def Delete Account():
+        //
+    def DeactivateAccount():
+        //
+    def ReasonSelection():
+        //
---
+++ TaxEkkr.py
@@ -0,0 +1,29 @@
+class PendingReservations:
+    travelList = []
+    review = str
+    starReview =  int
+    
+    def CreateForm():
+        //
+    def RequestForm():
+        //
+    def ReviwTraveler():
+        //
+    def WriteReview():
+        //
+    def StarReview():
+        //
+    def DeleteReservation():
+        //
+    def InformTraveler():
+        //
+    def CallPendingTravels():
+        //
+    def CompleteReservation():
+        //
+    def Confirm():
+        //
+    def InformTraveler():
+        //
+    
+    
