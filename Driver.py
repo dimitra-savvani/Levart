f11239a4ac971c715747862fb4a87049316a8a1b
JohnMpanias <64913152+JohnMpanias@users.noreply.github.com>
2020-05-23 15:48:14 +0300

    Add files via upload

---
+++ Driver.py
@@ -0,0 +1,13 @@
+class Driver:
+  startpoint: str
+  destination: str
+  date: date
+  time: time
+  luggages: str
+  pets: bool
+  
+  def FormFill():
+      
+  def DataCheck():
+      
+      return
---
+++ Vehicle.py
@@ -0,0 +1,11 @@
+class Vehicle:
+    vehicleType: typedef
+    vehicleBrand: typedef
+    transmissionType: typedef
+    fuelType: typedef
+    driveTrain: typedef
+  
+    def FormFill():
+      
+    def DataCheck():
+    return
