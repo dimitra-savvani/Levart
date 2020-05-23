2738f91b20287e258b5de07a92808f04282bcc27
NtoumiAris <64909886+NtoumiAris@users.noreply.github.com>
2020-05-23 13:23:10 +0300

    Add files via upload
    A first implementation of the classes "PanicButton" and "Passenger".

---
+++ PanicButton.py
@@ -0,0 +1,15 @@
+class PanicButton():
+    
+    emergencyContacts = "6996699669"
+    
+    activasionSignal = True
+    
+    manageList = ["I am in danger sample1" , "I am in danger sample2" , "I am in danger sample3"]
+    
+    def RequestPicture():
+        
+    def RequestLocationCapture():
+            
+    def RequestRightMessage():
+                
+    def RequestDangerList():
---
+++ Passenger.py
@@ -0,0 +1,17 @@
+class Passenger():
+    
+    startpoint = "Korinthou 17"
+    
+    destination = "Athens"
+    
+    date = "12/12/20"
+    
+    luggages = ["BackPack" , "Suitcase Small" , "Suitcase Medium" , "Siotcase Big" ]
+    
+    pets = False
+    
+    def FormFill():
+        
+    def DataCheck():
+            
+    def TravelSearch():
