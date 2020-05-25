c5e763a6f157817718ed7dda09d1b3438b5ddc96
JohnMpanias <64913152+JohnMpanias@users.noreply.github.com>
2020-05-25 21:59:29 +0300

    Update Vehicle.py

--- Vehicle.py
+++ Vehicle.py
@@ -1,11 +1,33 @@
 class Vehicle:
-    vehicleType: typedef
-    vehicleBrand: typedef
-    transmissionType: typedef
-    fuelType: typedef
-    driveTrain: typedef
-  
-    def FormFill():
+    vehicleType= ['Sedan', 'SUV', 'VAN', 'Pickup', 'HatchBack', 'Coupe', 'Cabriolet']
+    vehicleBrand= ['Mercedes', 'BMW', 'Audi', 'Subaru', 'Suzuki', 'Toyota', 'Opel']
+    transmissionType= ['Manual', 'Automatic']
+    fuelType= ['Gas', 'Petroleum', 'Diesel', 'Electric']
+    driveTrain= ['FWD', 'RWD', '4WD']
+
+    for i, j in enumerate(vehicleType):
+        print(i, j)
+        
+        ##a = input ("Τύπος αυτοκινήτου:")
+        ##print vehicleType(a).name
+        
+    for i, j in enumerate(vehicleBrand):
+        print(i, j)
+        ##b = input ("Μάρκα αυτοκινήτου:")
+        
+    for i, j in enumerate(transmissionType):
+        print(i, j)
+        ##c = input ("Σασμάν:")
+        
+    for i, j in enumerate(fuelType):
+        print(i, j)
+        ##d = input ("Καύσιμο:")
+        
+    for i, j in enumerate(driveTrain):
+        print(i, j)
+        ##e = input ("Κίνηση:")
+        
+   ##def FormFill():
       
-    def DataCheck():
-    return
+   ##def DataCheck():
+   ##return
