8ba15617f7a4befb87d2215b2fc1d32cc4fd6818
JohnMpanias <64913152+JohnMpanias@users.noreply.github.com>
2020-05-30 17:56:50 +0300

    Update Vehicle.py

--- Vehicle.py
+++ Vehicle.py
@@ -1,46 +1,90 @@
 class Vehicle:
-    vehicleType= ['Sedan', 'SUV', 'VAN', 'Pickup', 'HatchBack', 'Coupe', 'Cabriolet']
-    vehicleBrand= ['Mercedes', 'BMW', 'Audi', 'Subaru', 'Suzuki', 'Toyota', 'Opel']
-    transmissionType= ['Manual', 'Automatic']
-    fuelType= ['Gas', 'Petroleum', 'Diesel', 'Electric']
-    driveTrain= ['FWD', 'RWD', '4WD']
+    optionFill= ['Τύπος αυτοκινήτου', 'Μάρκα αυτοκινήτου', 'Σασμάν', 'Καύσιμο', 'Κίνηση']
+    f=999
+    
+    def VehicleForm(choice):
+        vehicleType= ['Sedan', 'SUV', 'VAN', 'Pickup', 'HatchBack', 'Coupe', 'Cabriolet']
+        vehicleBrand= ['Mercedes', 'BMW', 'Audi', 'Subaru', 'Suzuki', 'Toyota', 'Opel']
+        transmissionType= ['Manual', 'Automatic']
+        fuelType= ['Gas', 'Petroleum', 'Diesel', 'Electric']
+        driveTrain= ['FWD', 'RWD', '4WD']
+        
+        if choice==0:
+            a=999
+            for i, j in enumerate(vehicleType):
+                print(i, j)
+            while a<0 or a> len(vehicleType)-1:
+                try:
+                    a = int(input ("Τύπος αυτοκινήτου: "))                                  ##Select vehicle type
+                except:
+                    print ("Choice doesnt exist, please try again! ")
+                    print()    
+            print(vehicleType[a])
+            print()
+                
+        elif choice==1:
+            b=999
+            for i, j in enumerate(vehicleBrand):
+                print(i, j)
+            while b<0 or b> len(vehicleBrand)-1:
+                try:
+                    b = int(input ("Μάρκα αυτοκινήτου: "))                                  ##Select vehicle brand
+                except:                
+                    print ("Choice doesnt exist, please try again! ")
+                    print()
+            print(vehicleBrand[b])
+            print()
+            
+        elif choice==2:
+            c=999
+            for i, j in enumerate(transmissionType):
+                print(i, j)
+            while c<0 or c> len(transmissionType)-1:
+                try:
+                    c = int(input ("Σασμάν: "))                                             ##Select transmission type
+                except: 
+                    print ("Choice doesnt exist, please try again! ")
+                    print()              
+            print(transmissionType[c])
+            print()
 
-    for i, j in enumerate(vehicleType):
-        print(i, j)
-        
-    a = int(input ("Τύπος αυτοκινήτου: "))
-    print(vehicleType[a])
-    print()
+                                
+        elif choice==3:
+            d=999
+            for i, j in enumerate(fuelType):
+                print(i, j)
+            while d<0 or d> len(fuelType)-1: 
+                try:
+                    d = int(input ("Καύσιμο: "))                                            ##Select fuel type
+                except:
+                    print ("Choice doesnt exist, please try again! ")
+                    print()
+            print(fuelType[d])
+            print()
+
+                
+        elif choice==4:
+            e=999
+            for i, j in enumerate(driveTrain):
+                print(i, j)
+            while e<0 or e> len(driveTrain)-1:
+                try:
+                    e = int(input ("Κίνηση: "))                                             ##Select drivetrain
+                except:
+                    print ("Choice doesnt exist, please try again! ")
+                    print()
+            print(driveTrain[e])
+            print()
+                
+        else:
+            print ("Choice doesnt exist, please try again! ","Choose between 0 and 4.")
     
-    for i, j in enumerate(vehicleBrand):
-        print(i, j)
-        
-    b = int(input ("Μάρκα αυτοκινήτου: "))
-    print(vehicleBrand[b])
-    print()
-        
-    for i, j in enumerate(transmissionType):
-        print(i, j)
-        
-    c = int(input ("Σασμάν: "))
-    print(transmissionType[c])
-    print()
-        
-    for i, j in enumerate(fuelType):
-        print(i, j)
-        
-    d = int(input ("Καύσιμο: "))
-    print(fuelType[d])
-    print()
-        
-    for i, j in enumerate(driveTrain):
-        print(i, j)
-    
-    e = int(input ("Κίνηση: "))
-    print(driveTrain[e])
-    print()
-        
-   ##def FormFill():
-      
-   ##def DataCheck():
-   ##return
+    while f != 9:
+        for k,l in enumerate(optionFill):
+            print(k,l)
+        try:
+            f = int(input("Επιλογή πεδίου από 0 εως 4 αντίστοιχα: "))       
+            VehicleForm(f)
+        except:
+            print("Λάθος επιλογή! Διάλεξε μεταξύ 0 και 4.")
+            print()
