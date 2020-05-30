84779d736b36444ad4f660cc4067d115b56548c8
Dimitra Savvani <39067417+dimitra-savvani@users.noreply.github.com>
2020-05-30 15:58:30 +0300

    Προσθήκη exception στην μέθοδο LanguageSettings

--- LevartSettings.py
+++ LevartSettings.py
@@ -4,36 +4,41 @@ import PanicButton
 class LevartSettings:
     
      
-    
+    ''' Needed procedure to verify user'''
     def Identification(T):# with T being an object of class Traveler
-        changePic = int(input ('Πρόσθεσε νέα φωτογραφία προφίλ \n 0-->ΝΑΙ \n 1-->ΟΧΙ \n Επίλεξε:'))    
+        changePic = int(input ('Πρόσθεσε νέα φωτογραφία προφίλ \n 1-->ΝΑΙ \n 0-->ΟΧΙ \n Επίλεξε:'))    
         #upload eggrafo
         #elegxos
         #upload fwtografia
         #elgxos
-        #suugrisi
+        #sugrisi
         print('Επιτυχής ταυτοποίηση στοιχείων, μπορείτε πλέον να Ταξιδέψετε μέσω της Levart')
-        T.enableTraveling = True
-        return T.enableTraveling
+        T.enableTraveling = True #if true, users are allowed to arrange travels through Levart
+         
     
     
     def PanicButtonSettings(P):# with P being an object of class PanicButton
-         contactsList = [6983775649, 6947789379]
-         enablePanicButton = True
-         allowePictureCapture = True
-         allowLocationCapture = True
-         allowSendingMessage = True
+         P.contactsList = [6983775649, 6947789379]
+         P.enablePanicButton = True
+         P.allowePictureCapture = True
+         P.allowLocationCapture = True
+         P.allowSendingMessage = True
          
     def LangugeSettings(L):# with L being an object of class Levart
         Languages = ['Greek', 'English', 'German', 'French', 'Russian']
         for i, j in enumerate(Languages):
             print(i, j)
-        langIndex = int(input ("Διάλεξε Γλώσσα: "))
-        while langIndex > i :
-            langIndex = int(input ("Το νούμερο που διάλεξες δεν αντιστοιχεί σε διαθέσιμη γλώσσα, διάλεξε ξανά: "))
-        print('Η γλώσσα τέθηκε σε',Languages[langIndex])
-        L.languge = Languages[langIndex]
-        return L.language
+        while True:
+            try:
+                langIndex = int(input ("Διάλεξε Γλώσσα: "))
+                while langIndex > i :
+                    langIndex = int(input ("Το νούμερο που διάλεξες δεν αντιστοιχεί σε διαθέσιμη γλώσσα, διάλεξε ξανά: "))
+                print('Η γλώσσα τέθηκε σε',Languages[langIndex])
+                break
+            except ValueError:
+                print("Αυτό το πεδίο δέχετε μόνο αριθμούς")
+        L.language = Languages[langIndex]
+        
     
 
     def GeneralSettings(T):# with T being an object of class Traveler
@@ -41,3 +46,4 @@ class LevartSettings:
         
     def SecuritySettings(T):# with T being an object of class Traveler
         privateProfile = True
+
