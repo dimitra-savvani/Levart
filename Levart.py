0f70e016d59ed9140922863e3b7d4b92edcc6226
Dimitra Savvani <39067417+dimitra-savvani@users.noreply.github.com>
2020-05-31 17:37:09 +0300

    Ανάπτηξη του Register και του Login

--- Levart.py
+++ Levart.py
@@ -1,25 +1,60 @@
-class Levart:
+import Levart
+#import PanicButton
+
+class LevartSettings:
     
-    def Register(): 
-        typedUsername = input("Enter username ")
-        typedPassword = input("Enter password ")
-        Registered = True
-        return Registered 
+    
+     
+    ''' Needed procedure to verify user'''
+    def Identification(T):# with T being an object of class Traveler
+        changePic = int(input ('Πρόσθεσε νέα φωτογραφία προφίλ \n 1-->ΝΑΙ \n 0-->ΟΧΙ \n Επίλεξε:'))    
+        #upload eggrafo
+        #elegxos
+        #upload fwtografia
+        #elgxos
+        #sugrisi
+        print('Επιτυχής ταυτοποίηση στοιχείων, μπορείτε πλέον να Ταξιδέψετε μέσω της Levart')
+        T.enableTraveling = True #if true, users are allowed to arrange travels through Levart
+         
+    
+    
+    def PanicButtonSettings(P):# with P being an object of class PanicButton
+         P.contactsList = [6983775649, 6947789379]
+         P.enablePanicButton = True
+         P.allowePictureCapture = True
+         P.allowLocationCapture = True
+         P.allowSendingMessage = True
+         
+    def LanguageSettings(self):# with L being an object of class Levart
+        print('Διαλέξτε τη γλώσσα της επιλογής σας:')
+        Languages = ['Greek', 'English', 'German', 'French', 'Russian','Έξοδος απο ρυθμίσεις γλώσσας']
+        for i, j in enumerate(Languages):
+            print(i, j)
+        while True:
+            try:
+                langIndex = int(input ("Διάλεξε Γλώσσα: "))
+                while langIndex > i :
+                    langIndex = int(input ("Το νούμερο που διάλεξες δεν αντιστοιχεί σε διαθέσιμη γλώσσα, διάλεξε ξανά: "))
+                if langIndex == i:
+                    print('Έπιλέξατε έξοδο απο τις ρυθμίσεις γλώσσας')
+                else:
+                    print('Η γλώσσα τέθηκε σε',Languages[langIndex])
+                break
+            except ValueError:
+                print("Αυτό το πεδίο δέχετε μόνο αριθμούς.")
+        language = Languages[langIndex]
         
-    def Login():
-        typedUsername = input("Enter username ")
-        typedPassword = input("Enter password ")
-        correctLogin = True
-        return correctLogin  
     
-  
-    
-    def ComposeSMS(): 
-        text = 'some text'
-        return text
-    
-    #def SendMessage(): 
+
+    def GeneralSettings(T):# with T being an object of class Traveler
+        allowNotification = True
         
+    def SecuritySettings(T):# with T being an object of class Traveler
+        privateProfile = True
+        
+        
+        
+    
     
     
     
