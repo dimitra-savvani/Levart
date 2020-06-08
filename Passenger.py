73c1cac5bdbddeb4f8fdbbb2bb776ce32a6a9c70
NtoumiAris <64909886+NtoumiAris@users.noreply.github.com>
2020-06-08 18:27:22 +0300

    Υποψήφια τελική Passenger() κλάση.

--- Passenger.py
+++ Passenger.py
@@ -1,36 +1,19 @@
-from Traveler import Traveler
+from AggelosGit import Traveler
 class Passenger(Traveler):
     import glob
     import sys
     
     
-    #validinfo = False
-    #petscounter = 0
-    
-    #startpoint = "Κορίνθου 17"
-    
-    #destination = ""
-    
-    #date = '12/12/20'
-    
-    #luggages = ["Σακίδιο πλάτης" , "Μικ΄ρη βαλίτσα" , "Μεσαία βαλίτσα" , "Μεγάλη βαλίτσα"]
-    
-    #pets = ["Ναι" , "Οχι"]
-    
-    #people = ["1" , "2" , "3" ,"4"]
-    
-    
     
     def __init__(self,startpoint,destination,date,luggages,pets,people):
        
-        self.startpoint=startpoint
-        self.destination=destination
-        self.date=date
-        self.luggages=luggages
-        self.pets=pets
-        self.people=people
-       
-    
+        self.startpoint=startpoint #Σημείο έναρξης.
+        self.destination=destination #Σημείο άφιξης.
+        self.date=date #Ημερομηνία έναρξης ταξιδιού.
+        self.luggages=luggages #Αποσκευές που θα έχει ο χρήστης.
+        self.pets=pets #Κατοικίδια
+        self.people=people #Άτομα που θα εκτελέσουν το ταξίδι.
+          
     
     #Αυτή η μέθοδος θα δίνει τιμές στα πεδία της φόρμας.
     def FormFill(self):   
@@ -40,11 +23,22 @@ class Passenger(Traveler):
         
         self.startpoint = input("Διάλεξε το σημείο εκκίνησης:")
         self.destination = input("Επέλεξε προοριμσό:")
-        self.date=input("Πληκτρολογείστε την ημερομηνία έναρξης στην μορφή dd/mm/yyyy format: ")
-        day, month, year = list(map(int,self.date.split("/")))
-        self.date = datetime.date(year, month, day)
+        while True: #Έλεγχος ορθότητας ημερομηνιών. Δέχεται μόνο μεταγενέστερες ημερομηνίες απο την στιγμή που θα εκτελεστεί.
+                            try:
+                                self.date=input("Πληκτρολογείστε την ημερομηνία έναρξης στην μορφή dd/mm/yyyy: ") 
+                                day, month, year = list(map(int,self.date.split("/"))) 
+                                date = datetime.datetime(year, month, day) #ελεγχουμε αν εχει δωσει σωστες τιμες
+                                x = datetime.datetime.now()
+                                if  date >= x: 
+                                    False
+                                    break
+                                else:
+                                    print("Η χρονία δεν μπορεί να είναι μεγαλύτερη του 2020. Προσπαθήστε ξανά.")
+                                    True
+                            except ValueError:
+                                print("Η μορφή ημερομινίας που δώσατε δεν είναι αποδεκτή, παρακαλώ ακολουθήστε τις οδηγιές και προσπαθήστε ξανά.")
         
-        while True :  
+        while True :  #Έλεγχος ορθότητας εισόδου ύπαρξης κατοικιδίων.
             self.pets = input("Δηλώστε την ύπαρξη κατοικηδίου με Ναι η Οχι: " )
             if self.pets == "Ναι" or self.pets == "Οχι":
                False
@@ -52,15 +46,11 @@ class Passenger(Traveler):
             else:
                 print("Παρακαλώ πληκτρολογήστε ορθά την απάντηση σας!")           
         
-        while True :
+        while True : #Έλέγχος ορθότητας εισόδου αποσκευών.
             
             self.luggages = input("Επιλέξτε το μέγεθος των αποσκευών: \n Σακίδιο πλάτης , Μικρή βαλίτσα , Μεσαία βαλίτσα , Μεγάλη βαλίτσα:")
+            self.temp = self.luggages
             if self.luggages == "Σακίδιο πλάτης" or self.luggages == "Μικρή βαλίτσα" or self.luggages == "Μεσαία βαλίτσα" or self.luggages == "Μεγάλη βαλίτσα":
-                #if self.luggages == "Σακίδιο πλάτης":
-                    #print("Παρακαλώ δηλώστε την ύπαρξη βαλίτσας εάν αυτή υπάρχει.")
-                    #self.luggages=input("Παρακαλώ δηλώστε την ύπαρξη βαλίτσας εάν αυτή υπάρχει.\n Μικρή βαλίτσα , Μεσαία βαλίτσα , Μεγάλη βαλίτσα:")
-                #else:
-                    #self.luggages=input("Θα θέλατε θα έχετε μαζί σας και ένα Σακίδιο πλάτης:\n Ναι η Οχι")
                 False
                 break
             else :
@@ -68,14 +58,16 @@ class Passenger(Traveler):
               
         
         
-        while True:
+        while True: #Έλεγχος ορθότητας εισόδου αριθμού ατόμων.
          self.people = input("Παρακαλώ πληκτρολογήστε τον αριθμό τον ατόμων που επιθυμείτε \n1 2 3 4:")
          if self.people >= "1" and self.people <= "4" :
             False
             break               
          else:
             print("Παρακαλώ πληκτρολογήστε έναν αριθμό απο το 1 μέχρι και το 4")
-        # Μέθοδος που θα ελέγχει αν οι τύποι δεδομένων που συμπλήρωσε ο χρήστης είναι σωστοί.
+            
+            
+        # Μέθοδος που εκτυπώνει τα αποτελέσματα των εισόδων.
     def PrintInfo(self):
         
         print("Σημείο εκκίνησης:\n" , self.startpoint ,"\n", "Προορισμός:\n", self.destination ,"\n", "Ημερομηνία:\n" , self.date ,"\n" , "Αποσκευές:\n" , self.luggages, "\n" , "Κατοικίδια:\n", self.pets ,"\n", "Άτομα:\n" , self.people ,"\n")
@@ -88,7 +80,7 @@ class Passenger(Traveler):
              
     
    
-f=Passenger("αθηνα" , "πατρα" , "12/5/20" , "Βαλιτσα" , "Ναι" , "4")    
+f=Passenger("αθηνα" , "πατρα" , "12/5/20" , "Βαλιτσα" , "Ναι" , "4" )    
 f.FormFill()
 f.PrintInfo()       
    
