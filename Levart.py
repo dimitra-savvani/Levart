e3f151f28d556fa49218a19bd12102135f1a63a7
Dimitra Savvani <39067417+dimitra-savvani@users.noreply.github.com>
2020-06-08 23:13:40 +0300

    Βελτίωση σχετικά με το panic button

--- Levart.py
+++ Levart.py
@@ -1,105 +1,63 @@
-from myLevartSettings import LevartSettings
-from Traveler import Traveler
-import datetime
-class Levart:
-    L = LevartSettings()
-    language = L.LanguageSettings()
-    
-    companyInfo = ['261094735', '261094756', 'levart@info.gr']
-    
-    def __init__(self, language): 
-        self.language = language
-       
 
-    
-    def Register():
-        reg = int(input ('Κάνε εγγραφή στη Levart \n 0-->ΝΑΙ \n 1-->ΟΧΙ \n Επίλεξε:'))
-        if reg == 1:
-            typedΝame = input("Πλήκτρολογήστε το όνομα σας: ")
-            typedSurname = input("Πλήκτρολογήστε το επίθετο σας: ")
-            typedUsername = input("Πλήκτρολογήστε το όνομα χρήστη σας: ")
-            #it is acceptable only if not taken from another user
-            typedPassword1 = input("Πλήκτρολογήστε των κωδικό σας: ")
-            typedPassword2 = input("Πλήκτρολογήστε των κωδικό σας ξανά: ")
-            while typedPassword1 != typedPassword2:
-                print("Τα πεδία του κωδικού σας δεν ταιριάζουν, προσπαθηστε ξανα.")
-                typedPassword1 = input("Πλήκτρολογήστε των κωδικό σας: ")
-                typedPassword2 = input("Πλήκτρολογήστε των κωδικό σας ξανά: ")
-            k=True
-            while k:
-                try:
-                    typedBirhtdate=input("Πληκτρολογείστε τα νέa σας γενέθλια στην μορφή dd/mm/yyyy format: ")
-                    day, month, year = list(map(int, typedBirhtdate.split("/")))
-                    typedBirhtdate = datetime.date(year, month, day)
-                    if year>1900 and year<2020:
-                        k=False
-                    else:
-                        print("Η χρονία δεν μπορεί να είναι μικρότερη του 1900 και μεγαλύτερη του 2020. Προσπαθήστε ξανά.")
-                        k=True
-                except ValueError:
-                    print("Η μορφή ημερομινίας που δώσατε δεν είναι αποδεκτή, παρακαλώ ακολουθήστε τις οδηγιές και προσπαθήστε ξανά.")
-            k=True
-            while k:
-                typedTelephone=input("Πληκτρολογείστε το τηλέφωνο σας:")
-                for i in typedTelephone:
-                    try:
-                        int(typedTelephone)
-                        if len(typedTelephone)!=10:
-                            print("Ο αριθμός τηλεφώνου σας δεν μπορεί να είναι μικρότερος ή μεγαλύτερος από δέκα ψηφία. Παρακαλώ προσπαθήστε ξανά.")
-                            break
-                        else:
-                            k=False
-                    except ValueError:
-                        print("Επιτρέπονται μόνο αριθμοί στο τηλέφωνο σας.")
-                        break  
-            k=True
-            while k:
-                typedEmail=input("Πληκτρολογείστε το νέο σας email:")
-                for i in typedEmail:
-                    if i == ("")or i==(" "):
-                        print("Το στοιχείο",i,"δεν είναι επιτρεπτό. Δεν επιτρέποτνται κένα στο όνομα χρήστη σας.")
-                        break
-                    else:
-                        k=False
-                        break
-        
-        Registered = True
-        return Registered 
-        
-    def Login():
-        #Traveler(self.name, self.surname, self.username, self.password, self.birthdate, self.telephone, self.email)
-        T = Traveler("Mickey","Mouse","m","1","18/11/1928","6969696969","mickeymouse@upatras.com")
-        typedUsername = input("Πλήκτρολογήστε το όνομα χρήστη σας: ")
-        typedPassword = input("Πλήκτρολογήστε των κωδικό σας: ")
-        while not typedUsername == T.username or not typedPassword == T.password:
-            print("Τα στοιχεία που δώσατε δεν αντιστοιχούν σε κάποιο λογαριασμό.")
-            cancelLogin = input("Θέλετε να  αποχωρίσετε απο τη σελίδα του Loginq; y/n: ")
-            if cancelLogin == 'y':
-                return
-            typedUsername = input("Πλήκτρολογήστε το όνομα χρήστη σας: ")
-            typedPassword = input("Πλήκτρολογήστε των κωδικό σας: ")
-        print("Επιτυχής σύνδεση λογαριασμού.")
-        
-    
-    def ComposeSMS(): 
-        text = 'some text'
-        return text
-    
-    #def SendMessage(): 
-        
+#import PanicButton
+
+class LevartSettings:
     
     
+     
+    ''' Needed procedure to verify user'''
+    def Identification(T):# with T being an object of class Traveler
+        changePic = int(input ('Πρόσθεσε νέα φωτογραφία προφίλ \n 1-->ΝΑΙ \n 0-->ΟΧΙ \n Επίλεξε:'))    
+        #upload eggrafo
+        #elegxos
+        #upload fwtografia
+        #elgxos
+        #sugrisi
+        print('Επιτυχής ταυτοποίηση στοιχείων, μπορείτε πλέον να Ταξιδέψετε μέσω της Levart')
+        T.enableTraveling = True #if true, users are allowed to arrange travels through Levart
+         
     
     
-        
-        
-        
-    
-    
-    
-    
-    
-   
+    def PanicButtonSettings(P):# with P being an object of class PanicButton
+         typedEmergencyContacts = [6983775649, 6947789379]
+         typedEnablePanicButton = True
+         TypedAllowePictureCapture = True
+         TypedAllowLocationCapture = True
+         TypedAllowSendingMessage = True
+         
+         P.emergencyContacts = typedEmergencyContacts
+         P.enablePanicButton = typedEnablePanicButton
+         P.allowePictureCapture = TypedAllowePictureCapture
+         P.allowLocationCapture = TypedAllowLocationCapture
+         P.allowSendingMessage = TypedAllowSendingMessage
+         
+    def LanguageSettings(self):
+        print('Διαλέξτε τη γλώσσα της επιλογής σας:')
+        Languages = ['Greek', 'English', 'German', 'French', 'Russian','Έξοδος απο ρυθμίσεις γλώσσας']
+        for i, j in enumerate(Languages):
+            print(i, j)
+        while True:
+            try:
+                langIndex = int(input ("Διάλεξε Γλώσσα: "))
+                while langIndex > i :
+                    langIndex = int(input ("Το νούμερο που διάλεξες δεν αντιστοιχεί σε διαθέσιμη γλώσσα, διάλεξε ξανά: "))
+                if langIndex == i:
+                    print('Έπιλέξατε έξοδο απο τις ρυθμίσεις γλώσσας')
+                else:
+                    print('Η γλώσσα τέθηκε σε',Languages[langIndex])
+                break
+            except ValueError:
+                print("Αυτό το πεδίο δέχετε μόνο αριθμούς.")
+        language = Languages[langIndex]
+        return language
     
 
+    # def GeneralSettings(Τ):# with T being an object of class Traveler
+    #     allowNotification = True
         
+    # def SecuritySettings(T):# with T being an object of class Traveler
+    #     privateProfile = True
+        
+        
+        
+    
